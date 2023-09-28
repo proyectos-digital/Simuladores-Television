3ac4a693c3d88003fb2d5bae9ea047a79817bf71
Andrea Mejia <48025179+bbbackendvg@users.noreply.github.com>
2023-09-28 08:56:40 -0500

    Agrego la integración de la UI de los 7 casos

--- Assets/Scripts/MenuElementos.cs
+++ Assets/Scripts/MenuElementos.cs
@@ -5,60 +5,49 @@ using UnityEngine.SceneManagement;
 
 public class MenuElementos : MonoBehaviour
 {
-    [Header ("Activación de páneles")]
+    [Header ("Ui")]
     public GameObject panelMenu;
-    public GameObject panelLuces;
-    public GameObject panelMicrofonos;
+    public GameObject panelCamaras;
     public GameObject panelControles;
-    public GameObject panelGrabacion;
+    public GameObject modificaControles;
     public PlayerController playerController;
-
     private bool panelActivo;
-    public void ActivarMenu()
+    public void ActivarUI(int panel)
     {
         panelActivo = !panelActivo;
-        panelMenu.SetActive(panelActivo);
-        if(panelActivo == false)
+        switch (panel)
         {
-            panelLuces.SetActive(false);
-            panelMicrofonos.SetActive(false);
-            panelControles.SetActive(false);
-            playerController.LockCursor();
+            case 0:
+                panelMenu.SetActive(panelActivo);
+                if (panelActivo == true)
+                {
+                    playerController.moveSpeed = 0;
+                    playerController.mouseSensitivy = 0;
+                }
+                else
+                {
+                    playerController.moveSpeed = 5;
+                    playerController.mouseSensitivy = 2;
+                    playerController.LockCursor();
+                }
+                break;
+
+            case 1:
+                panelCamaras.SetActive(panelActivo);
+                break;
+
+            case 2:
+                panelControles.SetActive(panelActivo);
+                break;
+
+            case 3:
+                SceneManager.LoadScene("Menu");
+                break;
+
+            case 4:
+                modificaControles.SetActive(true);
+                panelControles.SetActive(false);
+                break;
         }
-    }
-    public void ActivarPanelLuces()
-    {
-        panelLuces.SetActive(true);
-        panelMicrofonos.SetActive(false);
-        panelControles.SetActive(false);
-    }
-
-    public void ActivarPanelMicroofonos()
-    {
-        panelLuces.SetActive(false);
-        panelMicrofonos.SetActive(true);
-        panelControles.SetActive(false);
-    }
-
-    public void ActivarControles()
-    {
-        panelLuces.SetActive(false);
-        panelMicrofonos.SetActive(false);
-        panelControles.SetActive(true);
-    }
-
-    public void ActivarPanelGrabacion()
-    {
-        panelActivo = !panelActivo;
-        panelGrabacion.SetActive(panelActivo);
-        if (!panelActivo)
-        {
-            playerController.LockCursor();
-        }
-    }
-
-    public void LoadMenu()
-    {
-        SceneManager.LoadScene("Menu");
     }
 }
